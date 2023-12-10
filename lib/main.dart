4c1f62d4bfc900c21e4ddfbf2b9366d55e4e703f
Mukund Solanki <mukundsolanki665@gmail.com>
2023-12-10 14:01:29 +0530

    File picker implementation'

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,95 @@
-import 'dart:io';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      title: 'File picker demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FilePickerDemo(title: 'File picker demo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class FilePickerDemo extends StatefulWidget {
+  const FilePickerDemo({super.key, required this.title});
+
+  final String title;
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<FilePickerDemo> createState() => _FilePickerDemoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  File? _selectedFile;
-
-  Future<void> _pickFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles();
-
-    if (result != null) {
-      setState(() {
-        _selectedFile = File(result.files.single.path!);
-      });
-    }
-  }
-
-  Future<void> _uploadFile() async {
-    if (_selectedFile == null) {
-      // Handle file not selected
-      return;
-    }
-
-    var request = http.MultipartRequest(
-      'POST',
-      Uri.parse('http://192.168.29.80:5500/upload'),
-    );
-
-    request.files.add(await http.MultipartFile.fromPath(
-      'file',
-      _selectedFile!.path,
-    ));
-
-    try {
-      final response = await request.send();
-      if (response.statusCode == 200) {
-        // File uploaded successfully
-        print('File uploaded!');
-      } else {
-        // Handle error
-        print('File upload failed with status: ${response.statusCode}');
-      }
-    } catch (e) {
-      // Handle exception
-      print('Error uploading file: $e');
-    }
-  }
-
+class _FilePickerDemoState extends State<FilePickerDemo> {
+  FilePickerResult? result;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('File Upload App'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: _pickFile,
-              child: Text('Pick a file'),
-            ),
-            SizedBox(height: 20),
-            if (_selectedFile != null)
-              Text('Selected File: ${_selectedFile!.path}'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _uploadFile,
-              child: Text('Upload File'),
-            ),
-          ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("File picker demo"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              if (result != null)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Selected file:',
+                        style:
+                            TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10,),
+                      ListView.separated(
+                          shrinkWrap: true,
+                          itemCount: result?.files.length ?? 0,
+                          itemBuilder: (context, index) {
+                            return Text(result?.files[index].name ?? '',
+                                style: const TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold));
+                          }, separatorBuilder: (BuildContext context, int index) { 
+                            return const SizedBox(height: 5,);
+                           },)
+                    ],
+                  ),
+                ),
+                const Spacer(),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    result =
+                        await FilePicker.platform.pickFiles(allowMultiple: true);
+                    if (result == null) {
+                      print("No file selected");
+                    } else {
+                      setState(() {});
+                      for (var element in result!.files) {
+                        print(element.name);
+                      }
+                    }
+                  },
+                  child: const Text("File Picker"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
