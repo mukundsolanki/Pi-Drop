7917e7f07f8c5bbc8a25fc2085c155599a46f257
Mukund Solanki <mukundsolanki665@gmail.com>
2023-12-10 15:33:33 +0530

    Floating action button for bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'package:http/http.dart' as http;
+import 'bottom_sheet.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,17 +21,15 @@ class MyApp extends StatelessWidget {
 }
 
 class FilePickerDemo extends StatefulWidget {
-  const FilePickerDemo({super.key, required this.title});
+  const FilePickerDemo({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<FilePickerDemo> createState() => _FilePickerDemoState();
+  _FilePickerDemoState createState() => _FilePickerDemoState();
 }
 
 class _FilePickerDemoState extends State<FilePickerDemo> {
-  FilePickerResult? result;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -47,103 +43,20 @@ class _FilePickerDemoState extends State<FilePickerDemo> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Center(
-                child: ElevatedButton(
-                  onPressed: () async {
-                    result =
-                        await FilePicker.platform.pickFiles(allowMultiple: true);
-                    if (result == null) {
-                      print("No file selected");
-                    } else {
-                      setState(() {});
-                      for (var element in result!.files) {
-                        print(element.name);
-                      }
-                    }
-                  },
-                  child: const Text("Pick File"),
-                ),
-              ),
-
-              const SizedBox(height: 20), // Add spacing between buttons
-              ElevatedButton(
-                onPressed: () {
-                  if (result != null) {
-                    _uploadFile(result!.files);
-                  } else {
-                    print("Please pick a file first");
-                  }
-                },
-                child: const Text("Upload"),
-              ),
-
-              if (result != null)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Selected file(s):',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      ListView.separated(
-                        shrinkWrap: true,
-                        itemCount: result?.files.length ?? 0,
-                        itemBuilder: (context, index) {
-                          return Text(
-                              result?.files[index].name ?? '',
-                              style: const TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold));
-                        },
-                        separatorBuilder:
-                            (BuildContext context, int index) {
-                          return const SizedBox(
-                            height: 5,
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ),
               const Spacer(),
+              
             ],
           ),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            BottomSheetWidget.show(context);
+          },
+          tooltip: 'Open Bottom Sheet',
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
     );
-  }
-
-  // Function to upload file to the server
-  void _uploadFile(List<PlatformFile> files) async {
-    try {
-      for (var file in files) {
-        var request = http.MultipartRequest(
-          'POST',
-          Uri.parse('http://192.168.29.192:3000/upload'),
-        );
-
-        request.files.add(await http.MultipartFile.fromPath(
-          'file',
-          file.path!,
-        ));
-
-        var response = await request.send();
-
-        if (response.statusCode == 200) {
-          print('File uploaded successfully');
-        } else {
-          print('Failed to upload file. Status code: ${response.statusCode}');
-        }
-      }
-    } catch (e) {
-      print('Error uploading file: $e');
-    }
   }
 }
