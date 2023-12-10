d1a087d853f10755a24f8d3f3cc5221d2739fe0a
Mukund Solanki <mukundsolanki665@gmail.com>
2023-12-10 14:19:49 +0530

    Basic structure with upload to server completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -31,6 +33,7 @@ class FilePickerDemo extends StatefulWidget {
 
 class _FilePickerDemoState extends State<FilePickerDemo> {
   FilePickerResult? result;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -44,33 +47,6 @@ class _FilePickerDemoState extends State<FilePickerDemo> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              if (result != null)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Selected file:',
-                        style:
-                            TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 10,),
-                      ListView.separated(
-                          shrinkWrap: true,
-                          itemCount: result?.files.length ?? 0,
-                          itemBuilder: (context, index) {
-                            return Text(result?.files[index].name ?? '',
-                                style: const TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold));
-                          }, separatorBuilder: (BuildContext context, int index) { 
-                            return const SizedBox(height: 5,);
-                           },)
-                    ],
-                  ),
-                ),
-                const Spacer(),
               Center(
                 child: ElevatedButton(
                   onPressed: () async {
@@ -85,13 +61,89 @@ class _FilePickerDemoState extends State<FilePickerDemo> {
                       }
                     }
                   },
-                  child: const Text("File Picker"),
+                  child: const Text("Pick File"),
                 ),
               ),
+
+              const SizedBox(height: 20), // Add spacing between buttons
+              ElevatedButton(
+                onPressed: () {
+                  if (result != null) {
+                    _uploadFile(result!.files);
+                  } else {
+                    print("Please pick a file first");
+                  }
+                },
+                child: const Text("Upload"),
+              ),
+
+              if (result != null)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Selected file(s):',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ListView.separated(
+                        shrinkWrap: true,
+                        itemCount: result?.files.length ?? 0,
+                        itemBuilder: (context, index) {
+                          return Text(
+                              result?.files[index].name ?? '',
+                              style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold));
+                        },
+                        separatorBuilder:
+                            (BuildContext context, int index) {
+                          return const SizedBox(
+                            height: 5,
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              const Spacer(),
             ],
           ),
         ),
       ),
     );
+  }
+
+  // Function to upload file to the server
+  void _uploadFile(List<PlatformFile> files) async {
+    try {
+      for (var file in files) {
+        var request = http.MultipartRequest(
+          'POST',
+          Uri.parse('http://192.168.29.192:3000/upload'),
+        );
+
+        request.files.add(await http.MultipartFile.fromPath(
+          'file',
+          file.path!,
+        ));
+
+        var response = await request.send();
+
+        if (response.statusCode == 200) {
+          print('File uploaded successfully');
+        } else {
+          print('Failed to upload file. Status code: ${response.statusCode}');
+        }
+      }
+    } catch (e) {
+      print('Error uploading file: $e');
+    }
   }
 }
