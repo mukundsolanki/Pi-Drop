c2ad033f0ada0a5934bd60cfeaf6157aee21a7af
Mukund Solanki <mukundsolanki665@gmail.com>
2023-12-11 19:34:36 +0530

    Download functionality for the files added

--- lib/details_widget.dart
+++ lib/details_widget.dart
@@ -1,64 +1,95 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
 
-class DetailsWidget extends StatelessWidget {
+class DetailsWidget extends StatefulWidget {
+  @override
+  _DetailsWidgetState createState() => _DetailsWidgetState();
+}
+
+class _DetailsWidgetState extends State<DetailsWidget> {
+  List<String> fileNames = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchFileNames();
+  }
+
+  Future<void> fetchFileNames() async {
+    try {
+      var url = Uri.parse('http://192.168.29.192:3000/files');
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        List<dynamic> data = json.decode(response.body);
+
+        if (data is List && data.every((element) => element is String)) {
+          setState(() {
+            fileNames = List<String>.from(data);
+          });
+        } else {
+          throw Exception('Invalid server response format');
+        }
+      } else {
+        throw Exception(
+            'Failed to fetch file names. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error fetching file names: $e');
+    }
+  }
+
+  Future<void> downloadFile(String fileName) async {
+    try {
+      var url = Uri.parse('http://192.168.29.192:3000/uploads/$fileName');
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        // Get the app's external storage directory
+        final directory = await getExternalStorageDirectory();
+        final filePath = '${directory!.path}/$fileName';
+
+        File file = File(filePath);
+        await file.writeAsBytes(response.bodyBytes);
+
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('File downloaded successfully: $filePath'),
+        ));
+      } else {
+        throw Exception(
+            'Failed to download file. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error downloading file: $e');
+    }
+  }
+
+  Future<void> _handleRefresh() async {
+    await fetchFileNames();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<String>>(
-      future: fetchFileNames(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return Text('No files available.');
-        } else {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Files on Server:',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(snapshot.data![index]),
-                  );
-                },
-              ),
-            ],
-          );
-        }
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('File Details'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: _handleRefresh,
+        child: ListView.builder(
+          itemCount: fileNames.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(Icons.file_download),
+              title: Text(fileNames[index]),
+              onTap: () => downloadFile(fileNames[index]),
+            );
+          },
+        ),
+      ),
     );
   }
-
-Future<List<String>> fetchFileNames() async {
-  try {
-    var url = Uri.parse('http://192.168.29.192:3000/files');
-    
-    var response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      // Parse the JSON response
-      List<dynamic> data = json.decode(response.body);
-      
-      if (data is List && data.every((element) => element is String)) {
-        return List<String>.from(data);
-      } else {
-        throw Exception('Invalid server response format');
-      }
-    } else {
-      throw Exception('Failed to fetch file names. Status code: ${response.statusCode}');
-    }
-  } catch (e) {
-    throw Exception('Error fetching file names: $e');
-  }
-}
 }
