d44fb132d8e8ba4cd6dcaca698dda1576f4545e6
Mukund Solanki <mukundsolanki665@gmail.com>
2024-01-29 12:33:10 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'File picker demo',
       theme: ThemeData(
         primarySwatch: Colors.teal,
